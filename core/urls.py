de17a6f2593338bf54c9bba9d12af8b794ce6d6d
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-04-01 22:19:07 +0300

    Some updates

--- core/urls.py
+++ core/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('checkout', checkout, name='checkout'),
     path('product/<slug>', ItemDetailView.as_view(), name='product'),
     path('add-to-cart/<slug>', add_to_cart, name='add-to-cart'),
+    path('remove-from-cart/<slug>', remove_from_cart, name='remove-from-cart'),
 ]
