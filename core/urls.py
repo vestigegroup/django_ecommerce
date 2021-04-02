2f625b1a482c164229d1a3e88afc508148c32d8f
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-04-02 13:25:39 +0300

    Create order-summary view
    Create urlpattern, template and view

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ app_name = 'core'
 urlpatterns = [
     path('', HomeView.as_view(), name='index'),
     path('checkout', checkout, name='checkout'),
+    path('order-summary', OrderSummaryView.as_view(), name='order-summary'),
     path('product/<slug>', ItemDetailView.as_view(), name='product'),
     path('add-to-cart/<slug>', add_to_cart, name='add-to-cart'),
     path('remove-from-cart/<slug>', remove_from_cart, name='remove-from-cart'),
--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 from django.utils import timezone
 from .models import Item, OrderItem, Order
 
@@ -16,6 +19,20 @@ class ItemDetailView(DetailView):
     template_name = 'product.html'
 
 
+class OrderSummaryView(LoginRequiredMixin, View):
+    def get(self, *args, **kwargs):
+        try:
+            order = Order.objects.get(user=self.request.user, ordered=False)
+            context = {
+                'object': order
+            }
+            return render(self.request, 'order-summary.html', context)
+        except ObjectDoesNotExist:
+            messages.error(self.request, 'You don\'t have active order.')
+            return redirect('/')
+
+
+@login_required
 def add_to_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
     order_item, created = OrderItem.objects.get_or_create(
@@ -44,6 +61,7 @@ def add_to_cart(request, slug):
         return redirect('core:product', slug=slug)
 
 
+@login_required
 def remove_from_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
     order_qs = Order.objects.filter(user=request.user, ordered=False)
