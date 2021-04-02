658a42374cf806535789bf3fd16979d8a9c9d98d
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-04-02 22:08:00 +0300

    Update form
    Add attributes and update template

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from  django_countries.fields import CountryField
+from django_countries.fields import CountryField
+from django_countries.widgets import CountrySelectWidget
 
 PAYMENT_OPTIONS = (
     ('S', 'Stripe'),
@@ -8,10 +9,20 @@ PAYMENT_OPTIONS = (
 
 
 class CheckoutForm(forms.Form):
-    street_address = forms.CharField()
-    apartment_address = forms.CharField(required=False)
-    country = CountryField(blank_label='(select country)')
-    zip = forms.CharField()
+    street_address = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': '1234 Main st'
+    }))
+    apartment_address = forms.CharField(required=False, widget=forms.TextInput(attrs={
+        'placeholder': 'Apartment or suite'
+    }))
+    country = CountryField(blank_label='(select country)').formfield(
+        required=False,
+        widget=CountrySelectWidget(attrs={
+            'class': 'custom-select d-block w-100',
+        }))
+    zip = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control'
+    }))
     same_billing_address = forms.BooleanField(widget=forms.CheckboxInput())
     save_info = forms.BooleanField(widget=forms.CheckboxInput())
     payment_option = forms.ChoiceField(widget=forms.RadioSelect, choices=PAYMENT_OPTIONS)
