8c55bf7eea44d256529c08f86efaa4182d1adf76
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-04-06 11:42:49 +0300

    Update tests.py
    Create tests for view

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Item
 
-# Create your tests here.
+
+class ViewTests(TestCase):
+    def test_home_view(self):
+        url = reverse('core:home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
