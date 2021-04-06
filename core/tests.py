dfdfa37dc32ffd5b4d4d6399b91f7dee4cd2ba87
Maxim Zavalniuk <mezgoodle@gmail.com>
2021-04-06 11:46:26 +0300

    Update tests.py
    Test Forms

--- core/tests.py
+++ core/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
-from .models import Item
+from .forms import PaymentForm
 
 
 class ViewTests(TestCase):
@@ -8,3 +8,15 @@ class ViewTests(TestCase):
         url = reverse('core:home')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
+
+
+class FormTests(TestCase):
+    def test_valid_form(self):
+        data = {'stripeToken': 'test-token', 'save': True, 'use_default': False}
+        form = PaymentForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_form(self):
+        data = {'stripeToken': 12, 'save': True, 'use_default': 'False'}
+        form = PaymentForm(data=data)
+        self.assertTrue(form.is_valid())
